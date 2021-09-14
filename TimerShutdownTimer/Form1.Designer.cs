30d0318ac29d3044bf523f86b52c699abfa0b4b5
yoni was <yonixw@gmail.com>
2021-09-15 01:20:09 +0300

    init 1

---
+++ TimerShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TimerShutdownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnAddTime = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.tbTime = new System.Windows.Forms.TrackBar();
+            this.tmrSub = new System.Windows.Forms.Timer(this.components);
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tbTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(558, 95);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(55, 34);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Toggle Topmost";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAddTime
+            // 
+            this.btnAddTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnAddTime.Location = new System.Drawing.Point(12, 95);
+            this.btnAddTime.Name = "btnAddTime";
+            this.btnAddTime.Size = new System.Drawing.Size(118, 34);
+            this.btnAddTime.TabIndex = 2;
+            this.btnAddTime.Text = "One time +";
+            this.btnAddTime.UseVisualStyleBackColor = false;
+            this.btnAddTime.Click += new System.EventHandler(this.btnAddTime_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCancel.Location = new System.Drawing.Point(136, 95);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(97, 34);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time Left:";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.Location = new System.Drawing.Point(472, 9);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(106, 13);
+            this.lblTimeLeft.TabIndex = 5;
+            this.lblTimeLeft.Text = "??m";
+            // 
+            // tbTime
+            // 
+            this.tbTime.Enabled = false;
+            this.tbTime.Location = new System.Drawing.Point(12, 45);
+            this.tbTime.Maximum = 500;
+            this.tbTime.Name = "tbTime";
+            this.tbTime.Size = new System.Drawing.Size(601, 45);
+            this.tbTime.TabIndex = 7;
+            this.tbTime.Value = 50;
+            // 
+            // tmrSub
+            // 
+            this.tmrSub.Tick += new System.EventHandler(this.tmrSub_Tick);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 154);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtPassword.Size = new System.Drawing.Size(601, 20);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.Text = "This is not the password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Shutdown Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Secret Token:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(29, 30);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "_";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 185);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.tbTime);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAddTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer shutdown helper";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAddTime;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.TrackBar tbTime;
+        private System.Windows.Forms.Timer tmrSub;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TimerShutdownTimer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TimerShutdownTimer.Properties;
+
+namespace TimerShutdownTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool disableClose = true;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (disableClose)
+                e.Cancel = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            disableClose = false;
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+        }
+
+        public int CurrentLeftSec = 60 * 60;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProcessTerminationProtection.ProcessProtect.ProtectCurrentProcess();
+            btnClose.Enabled = Settings.Default.CancelEnabled;
+            CurrentLeftSec = Settings.Default.TimerTimeMIN * 60;
+            tbTime.Maximum = CurrentLeftSec;
+            tbTime.Value = tbTime.Maximum;
+
+            btnAddTime.Text += Settings.Default.AddOneTimeMIN;
+
+            tmrSub.Interval = 1 * 1000;
+            tmrSub.Enabled = true;
+        }
+
+        private void OnBoom()
+        {
+            if(!Settings.Default.DryRun)
+            {
+                var psi = new ProcessStartInfo("shutdown", "/s /f /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+            else
+            {
+                MessageBox.Show("Dryrun Boom!");
+            }
+        }
+
+        bool isTimerEnabled = true;
+        private void tmrSub_Tick(object sender, EventArgs e)
+        {
+            if (isTimerEnabled)
+            {
+                CurrentLeftSec = Math.Max(0, CurrentLeftSec - (tmrSub.Interval / 1000));
+                if (CurrentLeftSec <= 2)
+                {
+                    isTimerEnabled = false;
+                    tbTime.Enabled = false;
+                    OnBoom();
+                }
+                else
+                {
+                    tbTime.Value = CurrentLeftSec;
+                    lblTimeLeft.Text = TimeSpan.FromSeconds(CurrentLeftSec).ToString();
+                }
+            }
+        }
+
+        DateTime lastTry = new DateTime();
+        bool isPasswordOK(string realpass)
+        {
+            DateTime now = DateTime.Now;
+            if ((now - lastTry).TotalSeconds >= 3)
+            {
+                lastTry = new DateTime();
+                if (realpass.Equals(txtPassword.Text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        bool added1Time = false;
+        private void btnAddTime_Click(object sender, EventArgs e)
+        {
+            if (!added1Time)
+            {
+                if (isPasswordOK(Settings.Default.AddOnPassword))
+                {
+                    tbTime.Maximum += Settings.Default.AddOneTimeMIN * 60;
+                    CurrentLeftSec += Settings.Default.AddOneTimeMIN * 60;
+                    added1Time = true;
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (isPasswordOK(Settings.Default.CancelPassword))
+            {
+                tmrSub.Enabled = false;
+                btnCancel.Enabled = false;
+                btnClose.Enabled = true;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OnBoom();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
